ace85c670750d1a17816e5a457283acadf98f79c
chairmp4 <ifd8503@gmail.com>
2025-03-26 13:15:16 -0400

    3/26 1:14pm
    Updated collectible code to call "random" spawn location which is calling the position of a child within a group of empty game objects. With that, all issues have been fixed!

--- Assets/__Scripts/CollectibleManager.cs
+++ Assets/__Scripts/CollectibleManager.cs
@@ -2,26 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
+public class CollectibleManager : MonoBehaviour
 {
     // enables rotation to be edited in the inspector
     public GameObject   collectiblePrefab;
-    public Vector3[]    exceptionRange;
+    [SerializeField] public GameObject[]    SpawnPoints;
     public float        rotationX;
     public float        rotationY;
     public float        rotationZ;
-    public float        spawnTotal;
-    // Update is called once per frame
 
+    // Update is called once per frame
     void Start()
     {   
-        // setting range for random spawn: X, Y, Z
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(19, 154), -9, Random.Range(-54, 75));
-        Instantiate(collectiblePrefab, randomSpawnPosition, Quaternion.identity);
+        {
+            int randomIndex = Random.Range(0, SpawnPoints.Length);
+            Debug.Log(randomIndex); // Relays index number into console
+
+            // Ensures no IndexOutOfRangeException error by checking value
+            if ( SpawnPoints.Length > 0 )
+            {
+                // Sets spawn location to randomIndex value called
+                Vector3 randomSpawnPosition = SpawnPoints[randomIndex].transform.position;
+                
+                // Spawns random spawn location
+                Instantiate(collectiblePrefab, randomSpawnPosition, Quaternion.identity);
+            }
+                
+        }
     }
 
     void Update()
     {
+        // Adds rotation to collectible asset that can be edited within the inspector
         transform.Rotate (new Vector3 (rotationX, rotationY, rotationZ) * Time.deltaTime);
     }
 }
