de7c4745d85afd920d92f91e0dd231aad1ebc102
chairmp4 <ifd8503@gmail.com>
2025-04-07 19:10:22 -0400

    Revert "4/4 1:01am"
    This reverts commit 8738ea3848bbd9ad25cdce680fd26abe5416f0a5.

--- Assets/Camera Controller/Assets/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Camera Controller/Assets/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -205,7 +205,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void GetInput(out float speed)
         {
             // Read input
-            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal Steer");
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
             float vertical = CrossPlatformInputManager.GetAxis("Vertical");
 
             bool waswalking = m_IsWalking;
